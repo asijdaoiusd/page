6280ada5894ea5a90633b0f60df74bc082f74ccf
asijdaoiusd <139229327+asijdaoiusd@users.noreply.github.com>
2023-07-11 08:29:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 var video = document.getElementById("video");
 var unmuteButton = document.getElementById("unmute-button");
 
+var videos = [
+  "https://cdn.discordapp.com/attachments/1107128683522703411/1128281353847320636/keoo.mp4",
+  "https://cdn.discordapp.com/attachments/1107128683522703411/1128256282239979550/y2mate.com_-_Kidd_Keo_24H_Official_Video__v720P.mp4"
+];
+
+var randomIndex = Math.floor(Math.random() * videos.length);
+var randomVideo = videos[randomIndex];
+video.src = randomVideo;
 
 unmuteButton.addEventListener("click", function() {
   if (video.muted) {
